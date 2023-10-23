ab1f4b720f8587979f70ec8a83a434eeac5033fb
Bogdan Keplast <bogdan.keplast@gmail.com>
2023-10-23 12:20:34 +0300

    SCRPT-16

---
+++ view/templates/layout.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="header clearfix">
+                <h2>Книжный магазин</h2>
+                <h4 class></h4>
+            </div>
+        </div>
+    </body>
+</html>
